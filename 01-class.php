aa3121189d2af16bf78e320c76bf6a9b7102ac9e
Estudiante <estuam@UAM.LOCAL>
2023-02-23 11:42:29 -0500

    feat: Files Construct & Private

--- 01-class.php
+++ 01-class.php
@@ -16,7 +16,7 @@
                        gap-5
                        bg-black/60
                        rounded-t-md">
-            <a href="index.html">
+            <a href="index.php">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10 text-yellow-200 hover:-translate-x-2 transition-all">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
